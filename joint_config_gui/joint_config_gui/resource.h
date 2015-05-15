b9b44c4124a3c2e7e213b180afdb76a9984ae9b1
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2015-05-15 19:23:21 +0900

    リポジトリ内容の移転

---
+++ joint_config_gui/joint_config_gui/resource.h
@@ -0,0 +1,37 @@
+#ifndef _RESOURCE_H_
+#define _RESOURCE_H_
+
+#define DIALOG_MAIN                             103
+#define RADIO_JOINT01                           40000
+#define RADIO_JOINT02                           40001
+#define RADIO_JOINT03                           40002
+#define RADIO_JOINT04                           40003
+#define RADIO_JOINT05                           40004
+#define RADIO_JOINT06                           40005
+#define RADIO_JOINT07                           40006
+#define RADIO_JOINT08                           40007
+#define RADIO_JOINT09                           40008
+#define RADIO_JOINT10                           40009
+#define RADIO_JOINT11                           40010
+#define RADIO_JOINT12                           40011
+#define RADIO_JOINT13                           40012
+#define RADIO_JOINT14                           40013
+#define RADIO_JOINT15                           40014
+#define RADIO_JOINT16                           40015
+#define RADIO_JOINT17                           40016
+#define RADIO_JOINT18                           40017
+#define BUTTON_MAX                              40018
+#define BUTTON_MIN                              40019
+#define BUTTON_UP                               40020
+#define BUTTON_DOWN                             40021
+#define BUTTON_HOME                             40022
+#define STEXT_ANGLE                             40023
+#define SLIDER_ANGLE                            40024
+#define LIST_COM                                40025
+#define EDIT_MAC                                40026
+#define BUTTON_COM_DISCONNECT                   40027
+#define BUTTON_PLEN2_DISCONNECT                 40028
+#define BUTTON_COM_CONNECT                      40029
+#define BUTTON_PLEN2_SCAN                       40030
+
+#endif // _RESOURCE_H_
